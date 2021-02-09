eea7394d7c3ccecbdf41455316528085a5933c9b
BERNARD Guillaume <34131124+GBernard314@users.noreply.github.com>
2021-02-09 15:45:18 +0100

    creating cool layout

--- app/src/main/java/fr/yapagi/stepbystep/MainActivity.kt
+++ app/src/main/java/fr/yapagi/stepbystep/MainActivity.kt
@@ -1,5 +1,6 @@
 package fr.yapagi.stepbystep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.FirebaseApp
@@ -9,12 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
         //Firebase stuff starts here
         FirebaseApp.initializeApp(this)
         val auth = Authenticator(this)
         //Logs user in as "JDoe" (Dummy values for test purposes)
         auth.login("john.doe@gmail.com", "password")
         //End of firebase stuff
+
+        startActivity(Intent(this, DashboardActivity::class.java))
     }
 }
