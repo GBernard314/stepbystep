04f4d48b69eb37a99a337e2af5dc327251fb5f5b
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-02-09 15:40:56 +0100

    Adds full connection procedure && tests with dummy values

--- app/src/main/java/fr/yapagi/stepbystep/MainActivity.kt
+++ app/src/main/java/fr/yapagi/stepbystep/MainActivity.kt
@@ -2,10 +2,19 @@ package fr.yapagi.stepbystep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import fr.yapagi.stepbystep.network.Authenticator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Firebase stuff starts here
+        FirebaseApp.initializeApp(this)
+        val auth = Authenticator(this)
+        //Logs user in as "JDoe" (Dummy values for test purposes)
+        auth.login("john.doe@gmail.com", "password")
+        //End of firebase stuff
     }
 }
