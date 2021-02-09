f759f1db3100d566211ea91f3fffa159a424b912
Perifanos <53171799+Perifanos@users.noreply.github.com>
2021-02-09 14:21:28 +0100

    Timer activity init

--- app/src/main/java/fr/yapagi/stepbystep/MainActivity.kt
+++ app/src/main/java/fr/yapagi/stepbystep/MainActivity.kt
@@ -1,11 +1,16 @@
 package fr.yapagi.stepbystep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.yapagi.stepbystep.timer.TimerActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intent = Intent(this, TimerActivity::class.java )
+        startActivity(intent)
     }
 }
