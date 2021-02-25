e84f34858455c75b1b97307ae6bbba9726e6c79a
BERNARD Guillaume <34131124+GBernard314@users.noreply.github.com>
2021-02-25 16:29:58 +0100

    adding barchart for week

--- app/src/main/java/fr/yapagi/stepbystep/DashboardActivity.kt
+++ app/src/main/java/fr/yapagi/stepbystep/DashboardActivity.kt
@@ -18,6 +18,10 @@ import fr.yapagi.stepbystep.databinding.ActivityDashboardBinding
 import fr.yapagi.stepbystep.map.MapActivity
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import fr.yapagi.stepbystep.timer.TimerActivity
@@ -175,6 +179,48 @@ class DashboardActivity : AppCompatActivity() {
                 actDisplay = activityTracking(listBarDataSet, barChart, actDisplay)
             }
         }
+        val barChart: BarChart = findViewById(R.id.activity);
+
+        val entries: ArrayList<BarEntry> = ArrayList()
+        entries.add( BarEntry(0f, 1000f));
+        entries.add( BarEntry(1f, 500f));
+        entries.add( BarEntry(2f, 564f))
+        entries.add( BarEntry(3f, 234f))
+        entries.add( BarEntry(4f, 700f))
+        entries.add( BarEntry(5f, 1500f))
+
+        val bardataset =  BarDataSet(entries, "Cells");
+        val days = ArrayList<String>()
+        days.add("Mon")
+        days.add("Tue")
+        days.add("Wed")
+        val act7 = listOf<Float>(1000f, 500f, 465f)
+
+        val labels = ArrayList<String>()
+        labels.add("2016");
+        labels.add("2015");
+        labels.add("2014");
+        labels.add("2013");
+        labels.add("2012");
+        labels.add("2011");
+
+        val dataB = BarData(bardataset);
+        barChart.data = dataB; // set the data and list of labels into chart
+        //barChart.setDescription("Set Bar Chart Description Here");  // set the description
+        barChart.legend
+        bardataset.color = R.color.activity_yellow
+        barChart.animateY(1000);
+        //barChart.xAxis.valueFormatter = LabelFormatter(days)
+        //barChart.setDescription("");    // Hide the description
+        barChart.axisLeft.setDrawLabels(false);
+        barChart.axisRight.setDrawLabels(false);
+        barChart.xAxis.setDrawLabels(false);
+
+        barChart.legend.isEnabled = false;   // Hide the legend
+        barChart.axisRight.setDrawGridLines(false);
+        barChart.axisLeft.setDrawGridLines(false);
+        barChart.xAxis.setDrawGridLines(false);
+        barChart.description.isEnabled = false;
 
         /*
             End of bar chart
