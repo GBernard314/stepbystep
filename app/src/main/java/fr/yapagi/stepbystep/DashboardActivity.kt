dde5b35be474712780b35b8c2ccee4b0b5629d7a
BERNARD Guillaume <34131124+GBernard314@users.noreply.github.com>
2021-02-25 16:29:58 +0100

    adding barchart for week

--- app/src/main/java/fr/yapagi/stepbystep/DashboardActivity.kt
+++ app/src/main/java/fr/yapagi/stepbystep/DashboardActivity.kt
@@ -1,17 +1,16 @@
 package fr.yapagi.stepbystep
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
-import com.github.mikephil.charting.utils.MPPointF
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import fr.yapagi.stepbystep.timer.TimerActivity
 
 
@@ -20,15 +19,20 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         val pieChart = findViewById<PieChart>(R.id.chart)
-        val NoOfEmp = ArrayList<PieEntry>()
+        
+        val NoOfStep = ArrayList<PieEntry>()
 
 
         val walked = 5000.toFloat()
         val goal = (15000 - walked)
 
-        NoOfEmp.add(PieEntry(walked, "steps"))
-        NoOfEmp.add(PieEntry(goal, "needed"))
-        val dataSet = PieDataSet(NoOfEmp, "Number Of Employees")
+
+        /*
+            Start of pie chart
+         */
+        NoOfStep.add(PieEntry(walked, "steps"))
+        NoOfStep.add(PieEntry(goal, "needed"))
+        val dataSet = PieDataSet(NoOfStep, "Number Of Employees")
 
         dataSet.setColors(intArrayOf(R.color.step_blue, R.color.background_grey), applicationContext)
 
@@ -52,6 +56,63 @@ class DashboardActivity : AppCompatActivity() {
         pieChart.invalidate()
         pieChart.animateXY(1000, 1000)
 
+        /*
+            End of pie chart
+         */
+
+        /*
+            Start of bar chart
+         */
+
+        val barChart: BarChart = findViewById(R.id.activity);
+
+        val entries: ArrayList<BarEntry> = ArrayList()
+        entries.add( BarEntry(0f, 1000f));
+        entries.add( BarEntry(1f, 500f));
+        entries.add( BarEntry(2f, 564f))
+        entries.add( BarEntry(3f, 234f))
+        entries.add( BarEntry(4f, 700f))
+        entries.add( BarEntry(5f, 1500f))
+
+        val bardataset =  BarDataSet(entries, "Cells");
+        val days = ArrayList<String>()
+        days.add("Mon")
+        days.add("Tue")
+        days.add("Wed")
+        val act7 = listOf<Float>(1000f, 500f, 465f)
+
+        val labels = ArrayList<String>()
+        labels.add("2016");
+        labels.add("2015");
+        labels.add("2014");
+        labels.add("2013");
+        labels.add("2012");
+        labels.add("2011");
+
+        val dataB = BarData(bardataset);
+        barChart.data = dataB; // set the data and list of labels into chart
+        //barChart.setDescription("Set Bar Chart Description Here");  // set the description
+        barChart.legend
+        bardataset.color = R.color.activity_yellow
+        barChart.animateY(1000);
+        //barChart.xAxis.valueFormatter = LabelFormatter(days)
+        //barChart.setDescription("");    // Hide the description
+        barChart.axisLeft.setDrawLabels(false);
+        barChart.axisRight.setDrawLabels(false);
+        barChart.xAxis.setDrawLabels(false);
+
+        barChart.legend.isEnabled = false;   // Hide the legend
+        barChart.axisRight.setDrawGridLines(false);
+        barChart.axisLeft.setDrawGridLines(false);
+        barChart.xAxis.setDrawGridLines(false);
+        barChart.description.isEnabled = false;
+
+        /*
+            End of bar chart
+         */
+
+
+
         //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TMP>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
         val tmpMap = findViewById<CardView>(R.id.cardView6)
         tmpMap.setOnClickListener{
@@ -59,5 +120,12 @@ class DashboardActivity : AppCompatActivity() {
             startActivity(intent)
         }
         //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TMP>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    }
+
+    class LabelFormatter(private val mLabels: ArrayList<String>) : IAxisValueFormatter {
+        override fun getFormattedValue(value: Float, axis: AxisBase): String {
+            return mLabels[value.toInt()]
+        }
+
     }
 }
