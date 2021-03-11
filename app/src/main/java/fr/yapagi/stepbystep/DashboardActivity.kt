4a1dbd7c7ee464a40d38423abe789cc56ebb9399
BERNARD Guillaume <34131124+GBernard314@users.noreply.github.com>
2021-03-11 10:12:57 +0100

    changing display working

--- app/src/main/java/fr/yapagi/stepbystep/DashboardActivity.kt
+++ app/src/main/java/fr/yapagi/stepbystep/DashboardActivity.kt
@@ -19,18 +19,24 @@ import fr.yapagi.stepbystep.map.MapActivity
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import fr.yapagi.stepbystep.databinding.ActivityDashboardBinding
 import fr.yapagi.stepbystep.timer.TimerActivity
 import fr.yapagi.stepbystep.tools.Tools
 
-private lateinit var binding : ActivityDashboardBinding
+private lateinit var binding: ActivityDashboardBinding;
+
+
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        binding = ActivityDashboardBinding.inflate(layoutInflater);
+        //setContentView(R.layout.activity_dashboard)
         setContentView(binding.root)
+
+
+
 
         val pieChart = findViewById<PieChart>(R.id.chart)
         val NoOfEmp = ArrayList<PieEntry>()
@@ -97,45 +103,49 @@ class DashboardActivity : AppCompatActivity() {
 
         val barChart: BarChart = findViewById(R.id.activity);
 
-        val entries: ArrayList<BarEntry> = ArrayList()
-        entries.add( BarEntry(0f, 1000f));
-        entries.add( BarEntry(1f, 500f));
-        entries.add( BarEntry(2f, 564f))
-        entries.add( BarEntry(3f, 234f))
-        entries.add( BarEntry(4f, 700f))
-        entries.add( BarEntry(5f, 1500f))
+        /*
+            changing display of barchart
+            0 = steps count
+            1 = calories
+         */
 
-        val bardataset =  BarDataSet(entries, "Cells");
-        val days = ArrayList<String>()
-        days.add("Mon")
-        days.add("Tue")
-        days.add("Wed")
-        val act7 = listOf<Float>(1000f, 500f, 465f)
+        val steps: ArrayList<BarEntry> = ArrayList()
+        steps.add( BarEntry(0f, 1000f));
+        steps.add( BarEntry(1f, 500f));
+        steps.add( BarEntry(2f, 564f))
+        steps.add( BarEntry(3f, 234f))
+        steps.add( BarEntry(4f, 700f))
+        steps.add( BarEntry(5f, 1500f))
+        steps.add( BarEntry(5f, 1500f))
 
-        val labels = ArrayList<String>()
-        labels.add("2016");
-        labels.add("2015");
-        labels.add("2014");
-        labels.add("2013");
-        labels.add("2012");
-        labels.add("2011");
+        val calories: ArrayList<BarEntry> = ArrayList()
+        calories.add( BarEntry(0f, 10f));
+        calories.add( BarEntry(1f, 5f));
+        calories.add( BarEntry(2f, 5f))
+        calories.add( BarEntry(3f, 2f))
+        calories.add( BarEntry(4f, 7f))
+        calories.add( BarEntry(5f, 15f))
+        calories.add( BarEntry(5f, 15f))
 
-        val dataB = BarData(bardataset);
-        barChart.data = dataB; // set the data and list of labels into chart
-        //barChart.setDescription("Set Bar Chart Description Here");  // set the description
-        barChart.legend
-        bardataset.color = R.color.activity_yellow
-        //barChart.xAxis.valueFormatter = LabelFormatter(days)
-        barChart.axisLeft.setDrawLabels(false);
-        barChart.axisRight.setDrawLabels(false);
-        barChart.xAxis.setDrawLabels(false);
+        var actDisplay = 0
+        //var bardataset = BarDataSet(steps, "Cells")
+        binding.actLogo.setOnClickListener {
 
-        barChart.legend.isEnabled = false;   // Hide the legend
-        barChart.axisRight.setDrawGridLines(false);
-        barChart.axisLeft.setDrawGridLines(false);
-        barChart.xAxis.setDrawGridLines(false);
-        barChart.description.isEnabled = false;
-        barChart.animateXY(1000, 1000)
+            /*
+                Managing the change of data to display
+             */
+            if (actDisplay == 0){
+                val bardataset =  BarDataSet(steps, "steps")
+                activityTracking(bardataset, barChart, actDisplay)
+
+                actDisplay = 1
+            } else {
+                val bardataset =  BarDataSet(calories, "Calories");
+                activityTracking(bardataset, barChart, actDisplay)
+
+                actDisplay = 0
+            }
+        }
 
         /*
             End of bar chart
@@ -160,6 +170,31 @@ class DashboardActivity : AppCompatActivity() {
         //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TMP>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     }
 }
+
+    fun activityTracking(barDataSet: BarDataSet, barChart: BarChart, state: Int){
+        // Changing name of card
+        if (state == 0){
+            binding.actText.text = "Activity : steps"
+        } else {
+            binding.actText.text = "Activity : Calories"
+        }
+        val dataB = BarData(barDataSet);
+        barChart.data = dataB; // set the data and list of labels into chart
+        //barChart.setDescription("Set Bar Chart Description Here");  // set the description
+        barChart.legend
+        barDataSet.color = R.color.activity_yellow
+        //barChart.xAxis.valueFormatter = LabelFormatter(days)
+        barChart.axisLeft.setDrawLabels(false);
+        barChart.axisRight.setDrawLabels(false);
+        barChart.xAxis.setDrawLabels(false);
+
+        barChart.legend.isEnabled = false;   // Hide the legend
+        barChart.axisRight.setDrawGridLines(false);
+        barChart.axisLeft.setDrawGridLines(false);
+        barChart.xAxis.setDrawGridLines(false);
+        barChart.description.isEnabled = false;
+        barChart.animateXY(500, 500)
+    }
 
     class LabelFormatter(private val mLabels: ArrayList<String>) : IAxisValueFormatter {
         override fun getFormattedValue(value: Float, axis: AxisBase): String {
